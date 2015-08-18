77a3f1b1d97be2945e9296dd97ebff2324595dac
pravinkul <pravin.kul@gmail.com>
2015-08-18 18:13:21 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,25 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(inputMatrix = matrix()) {
+      if(!is.matrix(inputMatrix)){
+      stop("Please give a correct matrix")
+      }
+      invertedMatrix <- NULL
 
+     set <-  function(y){
+     inputMatrix <<- y
+     invertedMatrix <<- NULL
+     }
 }
 
+get <- function() inputMatrix
+set.inverse <- function(solve) invertedMatrix <<- solve
+get.inverse <- function() invertedMatrix
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
 }
